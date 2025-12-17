421d62074a76a946a3c6229bcc43d5c1a3bc371a
范佳兴 <c1111111c@163.com>
2025-12-17 17:01:02 +0800

    feat:修改系统命名

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -16,6 +16,6 @@ public class RuoYiApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  大学生学籍管理系统启动成功   ლ(´ڡ`ლ)ﾞ");
+        System.out.println("(♥◠‿◠)ﾉﾞ  智慧用水行为分析管理系统启动成功   ლ(´ڡ`ლ)ﾞ");
     }
 }
